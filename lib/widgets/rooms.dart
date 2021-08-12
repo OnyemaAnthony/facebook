bcbdf0ed6cca36a514440ef53c998fad9af004b4
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-12 20:31:39 +0000

    make rooms responsive

--- lib/widgets/rooms.dart
+++ lib/widgets/rooms.dart
@@ -1,6 +1,7 @@
 import 'package:facebook/models/UserModel.dart';
 import 'package:facebook/utitlities.dart';
 import 'package:facebook/widgets/profile_avatar.dart';
+import 'package:facebook/widgets/responsive.dart';
 import 'package:flutter/material.dart';
 
 class Rooms extends StatelessWidget {
@@ -10,26 +11,34 @@ class Rooms extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: Colors.white,
-      child: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 10,horizontal:4  ),
-        scrollDirection: Axis.horizontal,
-        itemCount: 1+onlineUsers!.length,
-        itemBuilder: (context,index){
-          if(index==0){
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal:isDesktop?5:0 ),
+      elevation: isDesktop ? 1 : 0,
+      shape:  isDesktop? RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ):null,
+      child: Container(
+        height: 60,
+        color: Colors.white,
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 10,horizontal:4  ),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1+onlineUsers!.length,
+          itemBuilder: (context,index){
+            if(index==0){
+              return Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: _CreateRoomButton(),
+              );
+            }
+            final UserModel user = onlineUsers![index-1];
             return Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: _CreateRoomButton(),
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: ProfileAvatar(isActive: true,imageUr: user.imageUrl,),
             );
-          }
-          final UserModel user = onlineUsers![index-1];
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: ProfileAvatar(isActive: true,imageUr: user.imageUrl,),
-          );
-        },
+          },
+        ),
       ),
     );
   }
