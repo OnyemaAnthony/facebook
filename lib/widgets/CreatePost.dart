6152f74db8a2ace9802a9bbef36ede5b05c22133
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-12 20:30:05 +0000

    fix error in creat post

--- lib/widgets/CreatePost.dart
+++ lib/widgets/CreatePost.dart
@@ -15,8 +15,8 @@ class CreatePost extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal:isDesktop?5:0 ),
       elevation: isDesktop ? 1 : 0,
       shape:  isDesktop? RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10)
-      ),
+        borderRadius: BorderRadius.circular(10),
+      ):null,
       child: Container(
         padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
         color: Colors.white,
