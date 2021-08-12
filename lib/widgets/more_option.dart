997b40ae15c467186f56dfa971b3b5b6b8550fd7
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-12 21:21:53 +0000

    finish desktop size

--- lib/widgets/more_option.dart
+++ lib/widgets/more_option.dart
@@ -4,18 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MoreOptionsList extends StatelessWidget {
-  final List<List> _moreOptionsList = const[
-    [Icons.person_pin_circle_outlined, Colors.deepPurple,'COVID-19 Info Center'],
-    [Icons.people,Colors.cyan,'Friends'],
-    [FontAwesomeIcons.facebookMessenger,Utilities.facebookBlue,'Messenger'],
-    [FontAwesomeIcons.page4,Colors.orange,'Pages'],
-    [Icons.aspect_ratio_rounded,Utilities.facebookBlue,'Marketplace'],
-    [Icons.ondemand_video_rounded,Utilities.facebookBlue,'Watch'],
-    [Icons.event,Colors.red,'Events'],
-
+  final List<List> _moreOptionsList = const [
+    [
+      Icons.person_pin_circle_outlined,
+      Colors.deepPurple,
+      'COVID-19 Info Center'
+    ],
+    [Icons.people, Colors.cyan, 'Friends'],
+    [FontAwesomeIcons.facebookMessenger, Utilities.facebookBlue, 'Messenger'],
+    [FontAwesomeIcons.page4, Colors.orange, 'Pages'],
+    [Icons.aspect_ratio_rounded, Utilities.facebookBlue, 'Marketplace'],
+    [Icons.ondemand_video_rounded, Utilities.facebookBlue, 'Watch'],
+    [Icons.event, Colors.red, 'Events'],
   ];
 
   final UserModel? currentUser;
+
   MoreOptionsList({this.currentUser});
 
   @override
@@ -23,20 +27,20 @@ class MoreOptionsList extends StatelessWidget {
     return Container(
       constraints: BoxConstraints(maxWidth: 280),
       child: ListView.builder(
-        itemCount: 1+_moreOptionsList.length,
-          itemBuilder:(context,index){
-          if(index == 0){
+          itemCount: 1 + _moreOptionsList.length,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+              );
+            }
+            final List option = _moreOptionsList[index - 1];
             return Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child:
+                  _Option(icon: option[0], color: option[1], label: option[2]),
             );
-          }
-          final List option = _moreOptionsList[index-1];
-          return Padding(
-            padding:  const EdgeInsets.symmetric(vertical: 8),
-            child: _Option(icon:option[0],color:option[1],label:option[2]),
-          );
-          }
-      ),
+          }),
     );
   }
 }
@@ -45,22 +49,34 @@ class _Option extends StatelessWidget {
   final IconData? icon;
   final Color? color;
   final String? label;
-  _Option({this.color,this.label,this.icon});
+
+  _Option({this.color, this.label, this.icon});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-
-      },
+      onTap: () {},
       child: Row(
-      children: [
-        Icon(icon,size: 38,color: color,),
-        SizedBox(width: 6,),
-
-      ],
+        children: [
+          Icon(
+            icon,
+            size: 38,
+            color: color,
+          ),
+          SizedBox(
+            width: 6,
+          ),
+          Flexible(
+            child: Text(
+              label!,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
